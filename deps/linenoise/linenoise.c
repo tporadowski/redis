ada720788a32dad86ea0bc9f2de7f0b257dcad02
antirez <antirez@gmail.com>
2016-07-29 11:28:16 +0200

    Update linenoise to fix insecure redis-cli history file creation.
    The problem was fixed in antirez/linenoise repository applying a patch
    contributed by @lamby. Here the new version is updated in the Redis
    source tree.
    
    Close #1418
    Close #3322

--- deps/linenoise/linenoise.c
+++ deps/linenoise/linenoise.c
@@ -119,7 +119,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 #include "linenoise.h"
 
 #define LINENOISE_DEFAULT_HISTORY_MAX_LEN 100
@@ -1333,14 +1336,14 @@ int linenoiseHistorySetMaxLen(int len) {
 /* Save the history in the specified file. On success 0 is returned
  * otherwise -1 is returned. */
 int linenoiseHistorySave(const char *filename) {
-#ifdef _WIN32
-    FILE *fp = fopen(filename,"wb");
-#else
-    FILE *fp = fopen(filename,"w");
-#endif
+    mode_t old_umask = umask(S_IXUSR|S_IRWXG|S_IRWXO);
+    FILE *fp;
     int j;
 
+    fp = fopen(filename,"w");
+    umask(old_umask);
     if (fp == NULL) return -1;
+    chmod(filename,S_IRUSR|S_IWUSR);
     for (j = 0; j < history_len; j++)
         fprintf(fp,"%s\n",history[j]);
     fclose(fp);
