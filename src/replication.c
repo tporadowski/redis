cc81c041857f68691551054ca6a6920b2dcb69ad
Israel Lot <israel.lot@gmail.com>
2019-03-12 18:29:12 -0500

    Replicas aren't allowed to run the replicaof command

--- src/replication.c
+++ src/replication.c
@@ -2154,6 +2154,14 @@ void slaveofCommand(client *c) {
     } else {
         PORT_LONG port;
 
+		if (c->flags & CLIENT_SLAVE)
+		{
+			/* If a client is already a replica they cannot run this command,
+		 * because it involves flushing all replicas (including this client) */
+			addReplyError(c,"Command is not valid when client is a replica.");
+			return;
+		}
+
         if ((getLongFromObjectOrReply(c, c->argv[2], &port, NULL) != C_OK))
             return;
 
