d87a9c11a55bb93a280310aa5d04d1156b6e9b73
Israel Lot <israel.lot@gmail.com>
2019-03-12 18:13:15 -0500

    asyncCloseClientOnOutputBufferLimitReached(): don't free fake clients.

--- src/networking.c
+++ src/networking.c
@@ -2083,6 +2083,7 @@ int checkClientOutputBufferLimits(client *c) {
  * called from contexts where the client can't be freed safely, i.e. from the
  * lower level functions pushing data inside the client output buffers. */
 void asyncCloseClientOnOutputBufferLimitReached(client *c) {
+	if (c->fd == -1) return; /* It is unsafe to free fake clients. */
     POSIX_ONLY(serverAssert(c->reply_bytes < PORT_ULONG_MAX - (1024 * 64));)
         if (c->reply_bytes == 0 || c->flags & CLIENT_CLOSE_ASAP) return;
     if (checkClientOutputBufferLimits(c)) {
