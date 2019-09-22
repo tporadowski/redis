850b053074357768034721996b4adb0a4a4200e3
Tomasz Poradowski <tomasz.poradowski@generiscorp.com>
2019-09-22 10:13:49 +0200

    changed URL to issues page
    - most of crashes in this fork are specific to this Windows implementation
      so they should be reported in tporadowski/redis repo

--- src/debug.c
+++ src/debug.c
@@ -1112,7 +1112,7 @@ void sigsegvHandler(int sig, siginfo_t *info, void *secret) {
     serverLogRaw(LL_WARNING|LL_RAW,
 "\n=== REDIS BUG REPORT END. Make sure to include from START to END. ===\n\n"
 "       Please report the crash by opening an issue on github:\n\n"
-"           http://github.com/antirez/redis/issues\n\n"
+"           https://github.com/tporadowski/redis/issues\n\n"
 "  Suspect RAM error? Use redis-server --test-memory to verify it.\n\n"
 );
 
