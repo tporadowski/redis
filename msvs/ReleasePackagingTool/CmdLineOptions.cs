86a63b3efc0f38100e8027ab0959cdf2140b565c
tporadowski <tomasz.poradowski@gmail.com>
2020-01-26 11:36:09 +0100

    enhanced ReleasePackagingTool
    - added command-line arguments parser
    - support for "-n" that updates source file with new version

---
+++ msvs/ReleasePackagingTool/CmdLineOptions.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace ReleasePackagingTool
+{
+	class CmdLineOptions
+	{
+		[Option('n', "new-version", HelpText = "Prepares new version")]
+		public string PrepareNewVersion { get; set; }
+
+		[Option('p', "root-path", HelpText = "Root path to project folder")]
+		public string RootPath { get; set; }
+
+		[Option('s', "skip-package", HelpText = "When true skips preparing the package")]
+		public bool SkipPackage { get; set; }
+	}
+}
